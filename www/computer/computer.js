f09d68076240c6381f51e6f3e7413a645491fe68
valentinacataldo <100451232@alumnos.uc3m.es>
2023-04-18 11:03:08 +0200

    video player

--- www/computer/computer.js
+++ www/computer/computer.js
@@ -5,5 +5,13 @@ const socket = io("http://localhost:3000");
 socket.on("connect", () => {
     console.log("Connected to server");
     socket.emit("COM_CONNECTED", "Computer connected");
-    });
+    }
+);
 
+
+function player(video) {
+    document.querySelector(".wrapper").style.display = "none";
+    document.querySelector(".player").style.display = "block";
+    document.querySelector("#video").src = video;
+    document.querySelector("#video").play();
+}
